c0cf36e7f058a1d173d1d9a5e2a144f5b87aa2ed
Hoover <reallegofan@gmail.com>
2025-03-11 21:14:41 -0700

    test subtraction

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -1,22 +1,19 @@
 #include "unity.h"
 #include "calculator.h"
-
 // Optional but needs to be defined(runs BEFORE each test)
 void setUp(void) {
     // Initialize any resources needed for your tests here
     // For example, you might reset a global value, dynamically allocate memory, etc.
     // In this example, we do not need anything here.
 }
-
 // Optional but needs to be defined(runs AFTER each test)
 void tearDown(void) {
     // Clean up resources after each test
     // For example, free dynamically allocated memory, reset global values, etc.
     // In this example, we do not need anything here.
 }
-
 void test_add_positive_numbers(void) {
-    TEST_ASSERT_EQUAL(5, add(2, 3)); //We expect 2 + 3 to be 5
+    TEST_ASSERT_EQUAL(5, add(2, 3)); // We expect 2 + 3 to be 5
 }
 
 void test_add_positive_and_negative_numbers(void) {
@@ -29,8 +26,8 @@ void test_add_negative_numbers(void) {
 
 //NOTE: We can do multiple validations in one test
 void test_add_zero(void) {
-    TEST_ASSERT_EQUAL(10, add(10, 0));  //expect 10 + 0 = 10
-    TEST_ASSERT_EQUAL(0, add(0, 0));    //expect 0 + 0 = 0
+    TEST_ASSERT_EQUAL(10, add(10, 0)); //expect 10 + 0 = 10
+    TEST_ASSERT_EQUAL(0, add(0, 0)); //expect 0 + 0 = 0
 }
 
 void test_add_overflow(void) {
@@ -44,6 +41,18 @@ void test_add_underflow(void) {
     TEST_ASSERT_TRUE(result > 0);  //This checks if underflow occurred
 }
 
+void test_subtraction(void) {
+    TEST_ASSERT_EQUAL(-1, subtract(1, 2));
+}
+
+void test_multiply(void) {
+    TEST_ASSERT_EQUAL(6, multiply(2, 3));
+}
+
+void test_divide(void) {
+    TEST_ASSERT_EQUAL(3, divide(6, 2));
+}
+
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_add_positive_numbers);
@@ -52,5 +61,8 @@ int main(void) {
     RUN_TEST(test_add_zero);
     RUN_TEST(test_add_overflow);
     RUN_TEST(test_add_underflow);
+    RUN_TEST(test_subtraction);
+    //RUN_TEST(test_multiply);
+    //RUN_TEST(test_divide);
     return UNITY_END();
 }
